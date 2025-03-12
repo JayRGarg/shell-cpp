8f2d015ef53b1ad681097c7087a13440497619d9
JayRGarg <jgarg20@berkeley.edu>
2025-03-12 10:12:21 -0400

    implemented exit with different codes

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,21 @@ int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
+    std::string input;
 
-    while (true) {
-        std::cout << "$ ";
-        std::string input;
-        std::getline(std::cin, input);
+    bool exit = false;
+    int exit_status = 0;
+
+    std::cout << "$ ";
+
+    while (std::getline(std::cin, input)) {
+        std::cout << input << std::endl;
+        if (input.find("exit ") == 0) {
+            exit_status = std::stoi(input.substr(5, std::string::npos));
+            break;
+        }
         std::cout << input << ": command not found\n";
+        std::cout << "$ ";
     }
+    return exit_status;
 }
