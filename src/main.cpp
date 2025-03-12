f834797b07fd7127cffa52b6952c011d523f9342
JayRGarg <jgarg20@berkeley.edu>
2025-03-12 10:37:15 -0400

    implemented type

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+#include <unordered_set>
 
 int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    std::string input;
+    std::string input, rest;
+
+    std::unordered_set<std::string> valid_commands = {"exit", "echo", "type"};
 
     bool exit = false;
     int exit_status = 0;
@@ -17,6 +20,13 @@ int main() {
             break;
         } else if (input.find("echo ") == 0) {
             std::cout << input.substr(5, std::string::npos) << std::endl;
+        } else if (input.find("type ") == 0) {
+            rest = input.substr(5, std::string::npos);
+            if (valid_commands.contains(rest)) {
+                std::cout << rest << " is a shell builtin" << std::endl;
+            } else {
+                std::cout << rest << ": not found";
+            }
         } else {
             std::cout << input << ": command not found\n";
         }
